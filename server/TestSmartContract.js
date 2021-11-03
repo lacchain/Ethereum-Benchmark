e1667af053db229c5a455143a3e68dbce0bc944f
Eduardo Marchena <edumar111@gmail.com>
2021-11-02 21:28:57 -0500

    calculate nonce to  generated address- fix nonce

--- server/TestSmartContract.js
+++ server/TestSmartContract.js
@@ -1,6 +1,8 @@
 const {buildTransaction} = require('./pantheon_utils/web3Operations')
 const {sendTransactionAndProcessIncommingTx} = require("./lib/helpers")
+const {web3} =  require('./pantheon_utils/web3')
 const {test} = require("./Tester")
+const ethWallet = require('ethereumjs-wallet').default;
 const valueInEther = "0"
 let addressTo
 const {fileNameStimulus,fileNameResponse} = require('./ParametersSmartContractSending')
@@ -9,6 +11,7 @@ const {deploy} = require('./deployPublicSmartContract')
 const isContractAddress = process.env.SMART_CONTRACT_ADDRESS
 const {set} = require('./changeSmartContractState')
 let txData
+let addressFrom = '0xfe3b557e8fb62b89f4916b721be55ceb828dbd73'
 
 
 const getSmartContractParameters = async() => {
@@ -23,8 +26,13 @@ const getSmartContractParameters = async() => {
 }
 
 const publishSmartContractTransaction = async(privKey,t1,numberOfTransactions) => {
-  const txCount = 0 //await web3.eth.getTransactionCount(addressFrom)
-
+  // const privateKeyBuffer = EthUtil.toBuffer('0xf2ec77f749fc3a6552a4385ab043277463a8f359970dacc727a894db1c10d399');
+   const wallet = ethWallet.fromPrivateKey(privKey);
+   addressFrom = wallet.getAddressString()
+   // console.log('addres   : ' + addressFrom);
+  //const txCount = 0 
+  const txCount =await web3.eth.getTransactionCount(addressFrom)
+ // console.log("private key " + privKey)
   const txObject = buildTransaction(txCount,addressTo,valueInEther,txData)
   sendTransactionAndProcessIncommingTx(txObject,privKey,t1,fileNameResponse,numberOfTransactions)  
 }
