a02379224fa932865e1fc5fe88fc40c69947290a
Erick <eum602@gmail.com>
2021-12-01 17:55:18 -0500

    adds support to deploy identity manager and proxy contracts

--- server/changeSmartContractState.js
+++ server/changeSmartContractState.js
@@ -5,7 +5,7 @@ const sha3 = require("js-sha3").keccak_256
 const SMART_CONTRACT_OPTION  = process.env.SMART_CONTRACT_OPTION
 let set
 
-const nodeAddress = '0xd00e6624a73f88b39f82ab34e8bf2b4d226fd768';
+const nodeAddress = '0xc2d2aef92e0828e4420e7cb94bda2acad537c6ca' //'0xd00e6624a73f88b39f82ab34e8bf2b4d226fd768';
 const expiration = 1636394529;
 
 
@@ -48,11 +48,27 @@ const setDavid19 = () => { //Set For David-19 smart contract
   return txData
 }
 
+const setCreateIdentity = () => {
+  console.log("#######################Preparing value for IM 'createIdentity' smart contract stress test #######################")
+  const functionName = "createIdentity"
+  let set = web3.eth.abi.encodeFunctionSignature(`${functionName}()`)//function name to use
+  
+  const date = new Date()
+  const expiration =  Math.floor(date.getTime()/1000) + 5000000
+  let value = web3.eth.abi.encodeParameters(
+    ["address","uint256"],
+    [nodeAddress,expiration]);
+  const txData = set + value.substr(2);
+  return txData
+}
+
 const chooseSmartContractSetter = () => {
   if (SMART_CONTRACT_OPTION==0){
     set = setSimpleStorage
   }else if (SMART_CONTRACT_OPTION==1) {
     set = setDavid19
+  }else if (SMART_CONTRACT_OPTION == 2){
+    set = setCreateIdentity
   }
 }
 
